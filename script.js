36341dd34715955afa4249d95932cd50ce0e2287
Bryan Chow <bchow.post@gmail.com>
2023-05-19 17:17:23 +0100

    Complete Modal and Form

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 const myLibrary = [];
-const newTitle = "";
-const newAuthor = "";
-const newPages = "";
-const newRead = "";
-const addBook = document.querySelector("#new-book");
+let newTitle = "";
+let newAuthor = "";
+let newPages = "";
+let newRead = "";
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
 };
 
-function addBooktoLibrary() {
-    const newbook = new Book(newTitle, newAuthor, newPages, newRead)
-    myLibrary.push(newbook);
+function addBookToLibrary() {
+    let newBook = new Book(newTitle, newAuthor, newPages, newRead)
+    myLibrary.push(newBook);
 };
 
 function displayLibrary() {
 
 }
 
-addBook.addEventListener
+
+
+
+// Modal Code
+
+const modal = document.querySelector(".modal");
+const modalBtn = document.querySelector("#new-book");
+const span = document.querySelector(".close");
+
+
+modalBtn.onclick = () => {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Form Code
+
+const bookName = document.querySelector("#book-name");
+const authorName = document.querySelector("#author-name");
+const maxPages = document.querySelector("#max-pages");
+const readBoolean = document.querySelector("#read-boolean");
+const submitButton = document.querySelector("#submit");
+
+submitButton.onclick = (event) => {
+    newTitle = bookName.value
+    newAuthor = authorName.value
+    newPages = maxPages.value
+    newRead = readBoolean.checked
+    addBookToLibrary()
+    console.log(myLibrary);
+    modal.style.display = "none";
+    event.preventDefault()
+};
